eb3914c7909f2cbef88c69cbec8af6877f643931
itojun <itojun>
2000-04-27 11:16:54 +0000

    nuke strcpy, one more.

--- gencode.c
+++ gencode.c
@@ -21,7 +21,7 @@
  */
 #ifndef lint
 static const char rcsid[] =
-    "@(#) $Header: /tcpdump/master/libpcap/gencode.c,v 1.108 2000-04-27 11:16:19 itojun Exp $ (LBL)";
+    "@(#) $Header: /tcpdump/master/libpcap/gencode.c,v 1.109 2000-04-27 11:16:54 itojun Exp $ (LBL)";
 #endif
 
 #include <sys/types.h>
@@ -226,7 +226,7 @@ sdup(s)
 	int n = strlen(s) + 1;
 	char *cp = newchunk(n);
 
-	strcpy(cp, s);	/*safe*/
+	strlcpy(cp, s, n);
 	return (cp);
 }
 
