3e831d3cadd85f4c256e5720f666dbd7499f0603
guy <guy>
2002-04-20 21:05:02 +0000

    Don Lee was doing IP-over-FC, with the link-layer header from the capture device having only an RFC 2625 Network_Header field, not a Fibre Channel frame header; rename the constants to emphasize this and to leave room for another "raw Fibre Channel" link-layer type, if it's ever needed.

--- bpf/net/bpf.h
+++ bpf/net/bpf.h
@@ -37,7 +37,7 @@
  *
  *      @(#)bpf.h       7.1 (Berkeley) 5/7/91
  *
- * @(#) $Header: /tcpdump/master/libpcap/bpf/net/Attic/bpf.h,v 1.52.2.2 2002-04-20 10:02:56 guy Exp $ (LBL)
+ * @(#) $Header: /tcpdump/master/libpcap/bpf/net/Attic/bpf.h,v 1.52.2.3 2002-04-20 21:05:03 guy Exp $ (LBL)
  */
 
 #ifndef BPF_MAJOR_VERSION
@@ -331,10 +331,15 @@ struct bpf_hdr {
 #define DLT_HHDLC		121
 
 /*
- * Reserved for Fibre Channel, as per a request from Don Lee
- * <donlee@cray.com>.
+ * Reserved for RFC 2625 IP-over-Fibre Channel, as per a request from
+ * Don Lee <donlee@cray.com>.
+ *
+ * This is not for use with raw Fibre Channel, where the link-layer
+ * header starts with a Fibre Channel frame header; it's for IP-over-FC,
+ * where the link-layer header starts with an RFC 2625 Network_Header
+ * field.
  */
-#define DLT_FC			122
+#define DLT_IP_OVER_FC		122
 
 /*
  * The instruction encodings.
--- savefile.c
+++ savefile.c
@@ -30,7 +30,7 @@
 
 #ifndef lint
 static const char rcsid[] =
-    "@(#) $Header: /tcpdump/master/libpcap/savefile.c,v 1.55.2.3 2002-04-09 07:45:09 guy Exp $ (LBL)";
+    "@(#) $Header: /tcpdump/master/libpcap/savefile.c,v 1.55.2.4 2002-04-20 21:05:02 guy Exp $ (LBL)";
 #endif
 
 #ifdef HAVE_CONFIG_H
@@ -175,7 +175,7 @@ static const char rcsid[] =
 #define LINKTYPE_IPFILTER	116		/* IP Filter capture files */
 #define LINKTYPE_PFLOG		117		/* OpenBSD DLT_PFLOG */
 #define LINKTYPE_HHDLC		121		/* Siemens HiPath HDLC */
-#define LINKTYPE_FC		122		/* Fibre Channel */
+#define LINKTYPE_IP_OVER_FC	122		/* RFC 2625 IP-over-Fibre Channel */
 
 static struct linktype_map {
 	int	dlt;
@@ -254,8 +254,8 @@ static struct linktype_map {
 	/* Siemens HiPath HDLC */
 	{ DLT_HHDLC,		LINKTYPE_HHDLC },
 
-	/* Fibre Channel */
-	{ DLT_FC,		LINKTYPE_FC },
+	/* RFC 2625 IP-over-Fibre Channel */
+	{ DLT_IP_OVER_FC,	LINKTYPE_IP_OVER_FC },
 
 	/*
 	 * Any platform that defines additional DLT_* codes should:
