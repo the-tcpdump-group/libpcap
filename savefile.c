98bddc13841e3900dd6980fdbf93f6130e8b2638
guy <guy>
2005-06-03 20:36:19 +0000

    Add a "pcap_dump_ftell()" routine, to fetch the current offset of a pcap_dumper_t.  (Just doing an "ftell()" on the result of "pcap_dump_file()" won't necessarily work on Windows, as Microsoft, in their infinite wisdom, have multiple different versions of the C library runtime, and if a DLL is built using one version, and another DLL or an executable is built with another version, file descriptors and FILE *'s opened in one of them cannot be used in the other.)

--- savefile.c
+++ savefile.c
@@ -30,7 +30,7 @@
 
 #ifndef lint
 static const char rcsid[] _U_ =
-    "@(#) $Header: /tcpdump/master/libpcap/savefile.c,v 1.134 2005-05-23 20:30:44 guy Exp $ (LBL)";
+    "@(#) $Header: /tcpdump/master/libpcap/savefile.c,v 1.135 2005-06-03 20:36:20 guy Exp $ (LBL)";
 #endif
 
 #ifdef HAVE_CONFIG_H
@@ -1240,6 +1240,12 @@ FILE *
 pcap_dump_file(pcap_dumper_t *p)
 {
 	return ((FILE *)p);
+}
+
+long
+pcap_dump_ftell(pcap_dumper_t *p)
+{
+	return (ftell((FILE *)p));
 }
 
 int
