1ab594e82a11acc509c235f381d79aced1ce599e
guy <guy>
2008-08-06 07:53:47 +0000

    From Patrick McHardy:
    pcap-linux: fix invalid rcvbuf size
    
    Libpcap issues a SO_RCVBUF when the buffer size if unspecified (zero).
    The intention is to set it when its *not* zero.

--- pcap-linux.c
+++ pcap-linux.c
@@ -34,7 +34,7 @@
 
 #ifndef lint
 static const char rcsid[] _U_ =
-    "@(#) $Header: /tcpdump/master/libpcap/pcap-linux.c,v 1.129.2.25 2008-08-06 07:51:43 guy Exp $ (LBL)";
+    "@(#) $Header: /tcpdump/master/libpcap/pcap-linux.c,v 1.129.2.26 2008-08-06 07:53:47 guy Exp $ (LBL)";
 #endif
 
 /*
@@ -573,7 +573,7 @@ pcap_activate_linux(pcap_t *handle)
 		goto fail;
 	}
 
-	if (handle->opt.buffer_size == 0) {
+	if (handle->opt.buffer_size != 0) {
 		/*
 		 * Set the socket buffer size to the specified value.
 		 */
