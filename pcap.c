06af7782ae8082bdcefb63803f3868568c0542ff
Guy Harris <guy@alum.mit.edu>
2015-09-03 11:12:54 -0700

    Move the remaining send queue routines to pcap.c
    They're not platform-dependent, except perhaps to the extent that the
    only platform on which it's used is WinPcap, so they're inside "#ifdef
    _WIN32".

--- pcap.c
+++ pcap.c
@@ -1760,6 +1760,55 @@ pcap_oid_set_request_dead(pcap_t *p, pcap_oid_data_t *data)
 	return (PCAP_ERROR);
 }
 
+pcap_send_queue *
+pcap_sendqueue_alloc(u_int memsize)
+{
+	pcap_send_queue *tqueue;
+
+	/* Allocate the queue */
+	tqueue = (pcap_send_queue *)malloc(sizeof(pcap_send_queue));
+	if (tqueue == NULL){
+		return (NULL);
+	}
+
+	/* Allocate the buffer */
+	tqueue->buffer = (char *)malloc(memsize);
+	if (tqueue->buffer == NULL) {
+		free(tqueue);
+		return (NULL);
+	}
+
+	tqueue->maxlen = memsize;
+	tqueue->len = 0;
+
+	return (tqueue);
+}
+
+void 
+pcap_sendqueue_destroy(pcap_send_queue *queue)
+{
+	free(queue->buffer);
+	free(queue);
+}
+
+int 
+pcap_sendqueue_queue(pcap_send_queue *queue, const struct pcap_pkthdr *pkt_header, const u_char *pkt_data)
+{
+	if (queue->len + sizeof(struct pcap_pkthdr) + pkt_header->caplen > queue->maxlen){
+		return (-1);
+	}
+
+	/* Copy the pcap_pkthdr header*/
+	memcpy(queue->buffer + queue->len, pkt_header, sizeof(struct pcap_pkthdr));
+	queue->len += sizeof(struct pcap_pkthdr);
+
+	/* copy the packet */
+	memcpy(queue->buffer + queue->len, pkt_data, pkt_header->caplen);
+	queue->len += pkt_header->caplen;
+
+	return (0);
+}
+
 u_int
 pcap_sendqueue_transmit(pcap_t *p, pcap_send_queue *queue, int sync)
 {
