f01c8d38255685c7a3d8a506236d673bb147f2ed
Denis Ovsienko <denis@ovsienko.info>
2016-06-30 10:34:53 +0100

    refine the previous change to fix Linux builds

--- pcap-can-linux.c
+++ pcap-can-linux.c
@@ -133,7 +133,7 @@ can_create(const char *device, char *ebuf, int *is_ours)
 	/* OK, it's probably ours. */
 	*is_ours = 1;
 
-	p = pcap_create_common(device, ebuf, sizeof (struct pcap_can));
+	p = pcap_create_common(ebuf, sizeof (struct pcap_can));
 	if (p == NULL)
 		return (NULL);
 
--- pcap-usb-linux.c
+++ pcap-usb-linux.c
@@ -368,7 +368,7 @@ usb_create(const char *device, char *ebuf, int *is_ours)
 	/* OK, it's probably ours. */
 	*is_ours = 1;
 
-	p = pcap_create_common(device, ebuf, sizeof (struct pcap_usb_linux));
+	p = pcap_create_common(ebuf, sizeof (struct pcap_usb_linux));
 	if (p == NULL)
 		return (NULL);
 
