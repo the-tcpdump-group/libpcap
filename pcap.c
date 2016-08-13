2a2484949772f8d6724d9d54745de8bc3f12848d
Guy Harris <guy@alum.mit.edu>
2016-08-13 00:02:13 -0700

    Require that version.h be generated.
    All build procedures we support generate version.h (autoconf, CMake,
    MSVC), so always include it.  Get rid of all definitions of
    HAVE_VERSION_H, as we're not checking it any more.

--- pcap.c
+++ pcap.c
@@ -2132,23 +2132,7 @@ pcap_offline_filter(const struct bpf_program *fp, const struct pcap_pkthdr *h,
 		return (0);
 }
 
-/*
- * We make the version string static, and return a pointer to it, rather
- * than exporting the version string directly.  On at least some UNIXes,
- * if you import data from a shared library into an program, the data is
- * bound into the program binary, so if the string in the version of the
- * library with which the program was linked isn't the same as the
- * string in the version of the library with which the program is being
- * run, various undesirable things may happen (warnings, the string
- * being the one from the version of the library with which the program
- * was linked, or even weirder things, such as the string being the one
- * from the library but being truncated).
- */
-#ifdef HAVE_VERSION_H
 #include "version.h"
-#else
-static const char pcap_version_string[] = "libpcap version 1.x.y";
-#endif
 
 #ifdef _WIN32
 /*
