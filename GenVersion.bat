bb3258af5dc3d01acf717104d72d94b1fe36ccd2
Guy Harris <guy@alum.mit.edu>
2016-08-12 23:51:51 -0700

    Use the right parameters.
    Get rid of unneeded variables while we're at it.

--- GenVersion.bat
+++ GenVersion.bat
@@ -9,18 +9,13 @@ setlocal enableextensions disabledelayedexpansion
 set "search=%%%%LIBPCAP_VERSION%%%%"
 set /p replace=<%1
 
-set "inputTextFile=%2"
-set "outputTextFile=%3"
-
-echo Abuot to try to remove %3
 if exist %3 del %3 2>nul
-echo Removed %3 if it exists
 
-for /f "delims=" %%i in ('type %1' ) do (
+for /f "delims=" %%i in ('type %2' ) do (
 	set "line=%%i"
 	setlocal enabledelayedexpansion
 	set "line=!line:%search%=%replace%!"
-	>>%2 echo(!line!
+	>>%3 echo(!line!
 	endlocal
 )
 
