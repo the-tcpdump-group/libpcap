4f84547e1949b87fe455e42a3e8d174aa47b0d11
Ali Abdulkadir <autostart.ini@gmail.com>
2017-09-20 14:17:42 +0300

    Renamed Wspiapi.h to wspiapi.h

--- nametoaddr.c
+++ nametoaddr.c
@@ -57,7 +57,7 @@
      *
      * We use getaddrinfo(), so we include Wspiapi.h here.
      */
-    #include <Wspiapi.h>
+    #include <wspiapi.h>
   #endif /* INET6 */
 #else /* _WIN32 */
   #include <sys/param.h>
