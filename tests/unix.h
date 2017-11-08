13135782982e48ddaa6f788f8174c1df0f050886
Guy Harris <guy@alum.mit.edu>
2017-11-07 23:53:53 -0800

    More definitions to make Windows+MSVC more like UN*X.

--- tests/unix.h
+++ tests/unix.h
@@ -40,10 +40,19 @@
  * resemble.
  */
 #ifdef _MSC_VER
-  #define stat	_stat
-  #define fstat	_fstat
-  #define open	_open
-  #define close	_close
+  #define stat		_stat
+  #define fstat		_fstat
+
+  #define open		_open
+  #define O_RDONLY	_O_RDONLY
+  #define O_WRONLY	_O_WRONLY
+  #define O_RDWR	_O_RDWR
+  #define O_BINARY	_O_BINARY
+  #define O_CREAT	_O_CREAT
+  #define O_TRUNC	_O_TRUNC
+  #define read		_read
+  #define write		_write
+  #define close		_close
 #endif
 
 #endif
