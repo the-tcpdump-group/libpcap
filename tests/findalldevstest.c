579517faadd5ac5b169200c6a08916927d544328
Guy Harris <guy@alum.mit.edu>
2017-11-08 00:25:53 -0800

    OK, include winsock2.h, but before windows.h.

--- tests/findalldevstest.c
+++ tests/findalldevstest.c
@@ -6,8 +6,9 @@
 #include <string.h>
 #include <sys/types.h>
 #ifdef _WIN32
-  #include <windows.h>
+  #include <winsock2.h>
   #include <ws2ipdef.h>
+  #include <windows.h>
 #else
   #include <sys/socket.h>
   #include <netinet/in.h>
