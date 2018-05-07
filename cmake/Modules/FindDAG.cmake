4f0929f0947e4e95a33da216d6b6eb5a1872241b
sfd <stephen.donnelly@endace.com>
2018-05-07 16:33:30 +1200

    Add cmake support for libdagconf dependency

--- cmake/Modules/FindDAG.cmake
+++ cmake/Modules/FindDAG.cmake
@@ -7,18 +7,21 @@ find_path(DAG_INCLUDE_DIR dagapi.h)
 
 # Try to find the library
 find_library(DAG_LIBRARY dag)
+find_library(DAGCONF_LIBRARY dagconf)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(DAG
   DEFAULT_MSG
   DAG_INCLUDE_DIR
   DAG_LIBRARY
+  DAGCONF_LIBRARY
 )
 
 mark_as_advanced(
   DAG_INCLUDE_DIR
   DAG_LIBRARY
+  DAGCONF_LIBRARY
 )
 
 set(DAG_INCLUDE_DIRS ${DAG_INCLUDE_DIR})
-set(DAG_LIBRARIES ${DAG_LIBRARY})
+set(DAG_LIBRARIES ${DAG_LIBRARY} ${DAGCONF_LIBRARY})
