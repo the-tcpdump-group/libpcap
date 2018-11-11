c47806fd03c772846465015f3579fab4651e9ea8
Guy Harris <guy@alum.mit.edu>
2018-11-10 17:38:27 -0800

    Squelch an unreachable code warning.

--- rpcapd/log-stderr.c
+++ rpcapd/log-stderr.c
@@ -14,6 +14,19 @@ rpcapd_log(log_priority priority, const char *message, ...)
 	const char *tag;
 	va_list ap;
 
+	/*
+	 * Squelch warnings from compilers that *don't* assume that
+	 * priority always has a valid enum value and therefore don't
+	 * assume that we'll always go through one of the case arms.
+	 *
+	 * If we have a default case, compilers that *do* assume that
+	 * will then complain about the default case code being
+	 * unreachable.
+	 *
+	 * Damned if you do, damned if you don't.
+	 */
+	tag = "";
+
 	switch (priority) {
 
 	case LOGPRIO_INFO:
@@ -27,10 +40,6 @@ rpcapd_log(log_priority priority, const char *message, ...)
 	case LOGPRIO_ERROR:
 		tag = "error: ";
 		break;
-
-	default:
-		abort();
-		/* NOTREACHED */
 	}
 
 	fprintf(stderr, "rpcapd: %s", tag);
