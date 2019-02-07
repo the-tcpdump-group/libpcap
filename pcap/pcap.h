069919f901f7549feae74e287e792df35b7e5124
Anders Broman <a.broman58@gmail.com>
2019-02-07 09:49:02 +0100

    DPDK: Don't return fatal ERROR(-) on find_all_devices()  if DPDK is present but not configured.

--- pcap/pcap.h
+++ pcap/pcap.h
@@ -305,6 +305,7 @@ typedef void (*pcap_handler)(u_char *, const struct pcap_pkthdr *,
 #define PCAP_ERROR_CANTSET_TSTAMP_TYPE	-10	/* this device doesn't support setting the time stamp type */
 #define PCAP_ERROR_PROMISC_PERM_DENIED	-11	/* you don't have permission to capture in promiscuous mode */
 #define PCAP_ERROR_TSTAMP_PRECISION_NOTSUP -12  /* the requested time stamp precision is not supported */
+#define PCAP_ERROR_DPDK_NOT_INIT -13  /* DPDK not properly initialized */
 
 /*
  * Warning codes for the pcap API.
