68cc4c87f610951c5bd61dad2ce0f356bb762515
Guy Harris <guy@alum.mit.edu>
2019-04-25 11:12:13 -0700

    There is no SOCK_DEBUG_MESSAGE() macro, so don't call it.
    (cherry picked from commit 3e219077586b3303e015915cebd6b1e61b25ce0d)

--- sockutils.c
+++ sockutils.c
@@ -345,16 +345,13 @@ SOCKET sock_open(struct addrinfo *addrinfo, int server, int nconn, char *errbuf,
 	if (server)
 	{
 		/*
-		 * Allow a new server to bind the socket after the old one exited,
-		 * even if lingering sockets are still present.
+		 * Allow a new server to bind the socket after the old one
+		 * exited, even if lingering sockets are still present.
+		 *
+		 * Don't treat an error as a failure.
 		 */
 		int optval = 1;
-		if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0)
-		{
-			sock_geterror("socket(): ", errbuf, errbuflen);
-			/* dont treat an error as a failure */
-			SOCK_DEBUG_MESSAGE(errbuf);
-		}
+		(void)setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
 
 #if defined(IPV6_V6ONLY) || defined(IPV6_BINDV6ONLY)
 		/*
