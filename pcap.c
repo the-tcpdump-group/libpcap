550c4d8f372663d1b6617cad2279a1eca7cc58df
Guy Harris <guy@alum.mit.edu>
2019-09-08 15:25:48 -0700

    Always return a list of supported time-stamp types.
    In change 6e0c54a8f9640973e95ae755e6f60e1cceb7985f, we "have both the
    time stamp type-setting and precision-setting routines allow the default
    value (PCAP_TSTAMP_NONE/PCAP_TSTAMP_PRECISION_MICRO) if no list is
    supplied."  That means that, for example, "tcpdump -J" may report "Time
    stamp type cannot be set", but "tcpdump -j host" is allowed.  One could
    argue that you're not "setting" the time stamp, as "host" is the
    default, but it's confusing.
    
    This means that "host" will be reported as the one and only time stamp
    type if that's supported.

--- pcap.c
+++ pcap.c
@@ -384,8 +384,17 @@ pcap_list_tstamp_types(pcap_t *p, int **tstamp_typesp)
 	if (p->tstamp_type_count == 0) {
 		/*
 		 * We don't support multiple time stamp types.
+		 * That means the only type we support is PCAP_TSTAMP_HOST;
+		 * set up a list containing only that type.
 		 */
-		*tstamp_typesp = NULL;
+		*tstamp_typesp = (int*)malloc(sizeof(**tstamp_typesp));
+		if (*tstamp_typesp == NULL) {
+			pcap_fmt_errmsg_for_errno(p->errbuf, sizeof(p->errbuf),
+			    errno, "malloc");
+			return (PCAP_ERROR);
+		}
+		**tstamp_typesp = PCAP_TSTAMP_HOST;
+		return (1);
 	} else {
 		*tstamp_typesp = (int*)calloc(sizeof(**tstamp_typesp),
 		    p->tstamp_type_count);
@@ -396,8 +405,8 @@ pcap_list_tstamp_types(pcap_t *p, int **tstamp_typesp)
 		}
 		(void)memcpy(*tstamp_typesp, p->tstamp_type_list,
 		    sizeof(**tstamp_typesp) * p->tstamp_type_count);
+		return (p->tstamp_type_count);
 	}
-	return (p->tstamp_type_count);
 }
 
 /*
