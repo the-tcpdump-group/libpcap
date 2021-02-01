b7e3d6623074e56c57d4319a25989efa3f885ea7
Francois-Xavier Le Bail <devel.fx.lebail@orange.fr>
2021-02-01 22:59:18 +0100

    Travis CI: Build with less builders
    This change will save CI runtime.
    
    This will currently run five builders: amd64, arm64, ppc64le, s390x and osx.
    
    The build_matrix.sh script executes the matrix loops, exclude tests and
    cleaning.
    It calls the build.sh script which runs one build with exported variables
    setup: CC, CMAKE and REMOTE (default: gcc, no (cmake), disable (remote)).
    
    These scripts can easily be updated to run new tests (32 bits builds,
    sanitizers, coverage, etc).
    
    build_matrix.sh and build.sh can be used locally for build tests.
    
    Run examples:
    ./build_matrix.sh
    CC=clang ./build.sh
    CMAKE=yes ./build.sh
    CC=clang CMAKE=yes REMOTE=enable ./build.sh
    
    (With some Denis's ideas)
    
    Moreover:
    Remove the old workaround PATH=$PATH...
    Update the install directory prefix to /tmp/local.
    Use vim modeline in the two shell scripts.
    
    (cherry picked from commit a063c2d21417345ee583551ef2c07a0be6b32696)

---
+++ build_matrix.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# This script executes the matrix loops, exclude tests and cleaning.
+# It calls the build.sh script which runs one build with exported variables
+# setup: CC, CMAKE and REMOTE (default: gcc, no (cmake), disable (remote)).
+
+set -e
+
+# ANSI color escape sequences
+ANSI_MAGENTA="\\033[35;1m"
+ANSI_RESET="\\033[0m"
+# Install directory prefix
+PREFIX=/tmp/local
+
+travis_fold() {
+    local action="$1"
+    local name="$2"
+    if [ "$TRAVIS" != true ]; then return; fi
+    echo -ne "travis_fold:$action:$LABEL.script.$name\\r"
+    sleep 1
+}
+
+# Display text in magenta
+echo_magenta() {
+    echo -ne "$ANSI_MAGENTA"
+    echo "$@"
+    echo -ne "$ANSI_RESET"
+}
+
+touch .devel configure
+for CC in gcc clang; do
+    export CC
+    # Exclude gcc on OSX (it is just an alias for clang)
+    if [ "$CC" = gcc ] && [ "$TRAVIS_OS_NAME" = osx ]; then continue; fi
+    for CMAKE in no yes; do
+        export CMAKE
+        for REMOTE in disable enable; do
+            export REMOTE
+            echo_magenta "===== SETUP: compiler:$CC cmake:$CMAKE remote:$REMOTE ====="
+            # LABEL is needed to build the travis fold labels
+            LABEL="$CC.$CMAKE.$REMOTE"
+            # Run one build with exported variables setup: CC, CMAKE and REMOTE
+            ./build.sh
+            echo 'Cleaning...'
+            travis_fold start cleaning
+            if [ "$CMAKE" = yes ]; then rm -rf build; else make distclean; fi
+            rm -rf $PREFIX
+            git status -suall
+            git checkout .
+            travis_fold end cleaning
+        done
+    done
+done
+# vi: set tabstop=4 softtabstop=0 expandtab shiftwidth=4 smarttab autoindent :
