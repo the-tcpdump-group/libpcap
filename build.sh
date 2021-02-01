a063c2d21417345ee583551ef2c07a0be6b32696
Francois-Xavier Le Bail <devel.fx.lebail@orange.fr>
2021-02-01 22:59:18 +0100

    Travis CI: Build with less builders
    This change will save CI runtime.
    
    This will currently run five builders: amd64, arm64, ppc64le, s390x and osx.
    
    The build_matrix.sh script executes the matrix loops, exclude tests and
    cleaning.
    It calls the build.sh script which runs one build with exported variables
    setup: CC, CMAKE and REMOTE (default: gcc, no (cmake), disable (remote)).
    
    These scripts can easily be updated to run new tests (32 bits builds,
    sanitizers, coverage, etc).
    
    build_matrix.sh and build.sh can be used locally for build tests.
    
    Run examples:
    ./build_matrix.sh
    CC=clang ./build.sh
    CMAKE=yes ./build.sh
    CC=clang CMAKE=yes REMOTE=enable ./build.sh
    
    (With some Denis's ideas)
    
    Moreover:
    Remove the old workaround PATH=$PATH...
    Update the install directory prefix to /tmp/local.
    Use vim modeline in the two shell scripts.

---
+++ build.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# This script runs one build with exported variables setup: CC, CMAKE and
+# REMOTE (default: gcc, no (cmake), disable (remote)).
+
+set -e
+
+# CC: gcc or clang
+CC=${CC:-gcc}
+# CMAKE: no or yes
+CMAKE=${CMAKE:-no}
+# REMOTE: disable or enable
+REMOTE=${REMOTE:-disable}
+# Install directory prefix
+PREFIX=/tmp/local
+
+travis_fold() {
+    local action="$1"
+    local name="$2"
+    if [ "$TRAVIS" != true ]; then return; fi
+    echo -ne "travis_fold:$action:$LABEL.script.$name\\r"
+    sleep 1
+}
+
+# LABEL is needed to build the travis fold labels
+LABEL="$CC.$CMAKE.$REMOTE"
+if [ "$CMAKE" = yes ]; then
+    # ENABLE_REMOTE is only used by cmake
+    if [ "$REMOTE" = enable ]; then
+        ENABLE_REMOTE="-DENABLE_REMOTE=ON"
+    else
+        ENABLE_REMOTE=""
+    fi
+fi
+if [ "$CMAKE" = no ]; then
+    echo '$ ./configure [...]'
+    travis_fold start configure
+    ./configure --prefix=$PREFIX "--$REMOTE-remote"
+    travis_fold end configure
+else
+    [ ! -d build ] && mkdir build
+    cd build
+    echo '$ cmake [...]'
+    travis_fold start cmake
+    cmake -DCMAKE_INSTALL_PREFIX=$PREFIX "$ENABLE_REMOTE" ..
+    travis_fold end cmake
+fi
+make -s
+make -s testprogs
+if [ "$TRAVIS" = true ]; then
+    echo '$ make install [...]'
+    travis_fold start make_install
+    make install
+    travis_fold end make_install
+fi
+if [ "$CMAKE" = no ]; then
+    testprogs/findalldevstest
+else
+    run/findalldevstest
+fi
+if [ "$CMAKE" = no ]; then make releasetar; fi
+if [ "$TRAVIS" = true ]; then
+    echo '$ cat Makefile [...]'
+    travis_fold start cat_makefile
+    if [ "$CMAKE" = no ]; then
+        sed -n '1,/DO NOT DELETE THIS LINE -- mkdep uses it/p' < Makefile
+    else
+        cat Makefile
+    fi
+    travis_fold end cat_makefile
+    echo '$ cat config.h'
+    travis_fold start cat_config_h
+    cat config.h
+    travis_fold end cat_config_h
+    if [ "$CMAKE" = no ]; then
+        echo '$ cat config.log'
+        travis_fold start cat_config_log
+        cat config.log
+        travis_fold end cat_config_log
+    fi
+fi
+# vi: set tabstop=4 softtabstop=0 expandtab shiftwidth=4 smarttab autoindent :
