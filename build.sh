0cf22b51bfcd6a271a5e418639fc4c06f952bc50
Denis Ovsienko <denis@ovsienko.info>
2021-07-22 00:22:19 +0100

    CI: Port recent improvements from tcpslice. [skip appveyor]
    Start using build_common.sh, make default assignments early and
    uniformly, use Solaris-compatible command substitution, install bc on
    linux-amd64. This implements initial support for AIX and Solaris in the
    libpcap build matrix scripts.
    
    (cherry picked from commit 21daf3d59f1705de935dfa31bfc3abe2b0df6ab6)

--- build.sh
+++ build.sh
@@ -1,69 +1,44 @@
 #!/bin/sh -e
 
 # This script runs one build with setup environment variables: CC, CMAKE and
-# REMOTE (default: CC=gcc, CMAKE=no, REMOTE=no).
+# REMOTE.
+: "${CC:=gcc}"
+: "${CMAKE:=no}"
+: "${REMOTE:=no}"
 
-# CC: gcc or clang
-CC=${CC:-gcc}
-# GCC and Clang recognize --version and print to stdout. Sun compilers
-# recognize -V and print to stderr.
-"$CC" --version 2>/dev/null || "$CC" -V || :
-# CMAKE: no or yes
-CMAKE=${CMAKE:-no}
-# REMOTE: no or yes
-REMOTE=${REMOTE:-no}
+. ./build_common.sh
 # Install directory prefix
 if [ -z "$PREFIX" ]; then
-    PREFIX=$(mktemp -d -t libpcap_build_XXXXXXXX)
+    # shellcheck disable=SC2006
+    PREFIX=`mktempdir libpcap_build`
     echo "PREFIX set to '$PREFIX'"
     DELETE_PREFIX=yes
 fi
 
-# Run a command after displaying it
-run_after_echo() {
-    printf '$ '
-    echo "$@"
-    # shellcheck disable=SC2068
-    $@
-}
-
+print_cc_version
 if [ "$CMAKE" = no ]; then
-    echo '$ ./configure [...]'
-    ./configure --prefix="$PREFIX" --enable-remote="$REMOTE"
+    run_after_echo ./configure --prefix="$PREFIX" --enable-remote="$REMOTE"
 else
     # Remove the leftovers from any earlier in-source builds, so this
     # out-of-source build does not break because of that.
     # https://gitlab.kitware.com/cmake/community/-/wikis/FAQ#what-is-an-out-of-source-build
-    rm -rf CMakeFiles/ CMakeCache.txt
-    [ ! -d build ] && mkdir build
-    cd build
-    echo '$ cmake [...]'
-    cmake -DCMAKE_INSTALL_PREFIX="$PREFIX" -DENABLE_REMOTE="$REMOTE" ..
+    run_after_echo rm -rf CMakeFiles/ CMakeCache.txt
+    [ ! -d build ] && run_after_echo mkdir build
+    run_after_echo cd build
+    run_after_echo cmake -DCMAKE_INSTALL_PREFIX="$PREFIX" -DENABLE_REMOTE="$REMOTE" ..
 fi
-run_after_echo "make -s clean"
-run_after_echo "make -s"
-run_after_echo "make -s testprogs"
-echo '$ make install'
-make install
+run_after_echo make -s clean
+run_after_echo make -s
+run_after_echo make -s testprogs
+run_after_echo make install
 if [ "$CMAKE" = no ]; then
-    run_after_echo "testprogs/findalldevstest"
+    run_after_echo testprogs/findalldevstest
+    run_after_echo make releasetar
 else
-    run_after_echo "run/findalldevstest"
+    run_after_echo run/findalldevstest
 fi
-if [ "$CMAKE" = no ]; then
-    run_after_echo "make releasetar"
-fi
-if [ "$MATRIX_DEBUG" = true ]; then
-    echo '$ cat Makefile [...]'
-    sed '/^# DO NOT DELETE THIS LINE -- mkdep uses it.$/q' < Makefile
-    echo '$ cat config.h'
-    cat config.h
-    if [ "$CMAKE" = no ]; then
-        echo '$ cat config.log'
-        cat config.log
-    fi
-fi
+handle_matrix_debug
 if [ "$DELETE_PREFIX" = yes ]; then
-    rm -rf "$PREFIX"
+    run_after_echo rm -rf "$PREFIX"
 fi
 # vi: set tabstop=4 softtabstop=0 expandtab shiftwidth=4 smarttab autoindent :
--- build_matrix.sh
+++ build_matrix.sh
@@ -1,54 +1,52 @@
 #!/bin/sh -e
 
 # This script executes the matrix loops, exclude tests and cleaning.
-# The matrix can be configured with environment variables MATRIX_CC,
-# MATRIX_CMAKE and MATRIX_REMOTE (default: MATRIX_CC='gcc clang',
-# MATRIX_CMAKE='no yes', MATRIX_REMOTE='no yes').
+# The matrix can be configured with the following environment variables: MATRIX_CC,
+# MATRIX_CMAKE and MATRIX_REMOTE.
+: "${MATRIX_CC:=gcc clang}"
+: "${MATRIX_CMAKE:=no yes}"
+: "${MATRIX_REMOTE:=no yes}"
 # It calls the build.sh script which runs one build with setup environment
-# variables : CC, CMAKE and REMOTE (default: CC=gcc, CMAKE=no, REMOTE=no).
+# variables: CC, CMAKE and REMOTE.
 
-uname -a
-date
+. ./build_common.sh
+print_sysinfo
 # Install directory prefix
 if [ -z "$PREFIX" ]; then
-    PREFIX=$(mktemp -d -t libpcap_build_matrix_XXXXXXXX)
+    # shellcheck disable=SC2006
+    PREFIX=`mktempdir libpcap_build_matrix`
     echo "PREFIX set to '$PREFIX'"
     export PREFIX
 fi
 COUNT=0
 
-# Display text in magenta
-echo_magenta() {
-    printf '\033[35;1m' # ANSI magenta
-    echo "$@"
-    printf '\033[0m' # ANSI reset
-}
-
 touch .devel configure
-for CC in ${MATRIX_CC:-gcc clang}; do
+for CC in $MATRIX_CC; do
     export CC
     # Exclude gcc on macOS (it is just an alias for clang).
-    if [ "$CC" = gcc ] && [ "$(uname -s)" = Darwin ]; then
+    # shellcheck disable=SC2006
+    if [ "$CC" = gcc ] && [ "`uname -s`" = Darwin ]; then
         echo '(skipped)'
         continue
     fi
-    for CMAKE in ${MATRIX_CMAKE:-no yes}; do
+    for CMAKE in $MATRIX_CMAKE; do
         export CMAKE
-        for REMOTE in ${MATRIX_REMOTE:-no yes}; do
+        for REMOTE in $MATRIX_REMOTE; do
             export REMOTE
-            COUNT=$((COUNT+1))
+            # shellcheck disable=SC2006
+            COUNT=`increment $COUNT`
             echo_magenta "===== SETUP $COUNT: CC=$CC CMAKE=$CMAKE REMOTE=$REMOTE ====="
             # Run one build with setup environment variables: CC, CMAKE and REMOTE
-            ./build.sh
+            run_after_echo ./build.sh
             echo 'Cleaning...'
             if [ "$CMAKE" = yes ]; then rm -rf build; else make distclean; fi
-            rm -rf "${PREFIX:?}"/*
-            git status -suall
+            purge_directory "$PREFIX"
+            run_after_echo git status -suall
             # Cancel changes in configure
-            git checkout configure
+            run_after_echo git checkout configure
         done
     done
 done
-rm -rf "$PREFIX"
+run_after_echo rm -rf "$PREFIX"
 echo_magenta "Tested setup count: $COUNT"
 # vi: set tabstop=4 softtabstop=0 expandtab shiftwidth=4 smarttab autoindent :
