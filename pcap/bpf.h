803964cc764758f26aa7fcb08643561948bf5f38
Kristof Provost <kp@FreeBSD.org>
2021-12-13 16:21:43 +0100

    Improve bpf.h header conflict handling
    We carefully check for various operating system include guards to ensure
    that we include either the OS header or our own.
    This works fine if the OS header is included first, but if we include
    our own first and then the OS header we run into duplicate definitions.
    
    This fixes part of the build issues caused by recent FreeBSD including
    <net/bpf.h> from <net/if_pflog.h>.

--- pcap/bpf.h
+++ pcap/bpf.h
@@ -78,6 +78,10 @@
  */
 #if !defined(_NET_BPF_H_) && !defined(_NET_BPF_H_INCLUDED) && !defined(_BPF_H_) && !defined(_H_BPF) && !defined(lib_pcap_bpf_h)
 #define lib_pcap_bpf_h
+#define _NET_BPF_H_
+#define _NET_BPF_H_INCLUDED
+#define _BPF_H_
+#define _H_BPF
 
 /* u_char, u_short and u_int */
 #if defined(_WIN32)
