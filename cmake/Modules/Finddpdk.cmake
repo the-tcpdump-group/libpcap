17f87963b837a397d2d4d6d3f31848d87dd76072
Guy Harris <gharris@sonic.net>
2022-03-23 13:51:50 -0700

    configure, cmake: only check for DPDK using pkg-config.
    We only try to find DPDK using pkg-config; DPDK is *SO* complicated -
    DPDK 19.02, for example, has about 117(!) libraries, and the precise set
    of libraries required has changed ver time - so attempting to guess
    which libraries you need, and hardcoding that in an attempt to find the
    libraries without DPDK, rather than relying on DPDK to tell you, with a
    .pc file, what libraries are needed, is *EXTREELY* fragile and has
    caused some bug reports, so we're just not going to do it.
    
    If that causes a problem, the only thing we will do is accept an
    alternative way of finding the appropriate library set for the installed
    version of DPDK that is as robust as pkg-config (i.e., it had better
    work as well as pkg-config with *ALL* versions of DPDK that provide a
    libdpdk.pc file).
    
    Also, try to give as much information to the user as possible if we fail
    to find a usable DPDK.
    
    (cherry picked from commit 2106bb9999ff024908aa67ad1cbb9915cf145131)

--- cmake/Modules/Finddpdk.cmake
+++ cmake/Modules/Finddpdk.cmake
@@ -6,85 +6,26 @@
 # dpdk_INCLUDE_DIRS
 # dpdk_LIBRARIES
 
+#
+# We only try to find DPDK using pkg-config; DPDK is *SO*
+# complicated - DPDK 19.02, for example, has about 117(!)
+# libraries, and the precise set of libraries required has
+# changed ver time  - so attempting to guess which libraries
+# you need, and hardcoding that in an attempt to find the
+# libraries without DPDK, rather than relying on DPDK to
+# tell you, with a .pc file, what libraries are needed,
+# is *EXTREELY* fragile and has caused some bug reports,
+# so we're just not going to do it.
+#
+# If that causes a problem, the only thing we will do is
+# accept an alternative way of finding the appropriate
+# library set for the installed version of DPDK that is
+# as robust as pkg-config (i.e., it had better work as well
+# as pkg-config with *ALL* versions of DPDK that provide a
+# libdpdk.pc file).
+#
 if(PKG_CONFIG_FOUND)
   pkg_check_modules(dpdk QUIET libdpdk)
-endif()
-message(STATUS "Executing Finddpdk")
-if(NOT dpdk_INCLUDE_DIRS)
-  message(STATUS "Executing find_path")
-  find_path(dpdk_config_INCLUDE_DIR rte_config.h
-    HINTS
-      ENV DPDK_DIR
-    PATH_SUFFIXES
-      dpdk
-      include
-)
-  find_path(dpdk_common_INCLUDE_DIR rte_common.h
-    HINTS
-      ENV DPDK_DIR
-    PATH_SUFFIXES
-      dpdk
-      include
-)
-  set(dpdk_INCLUDE_DIRS "${dpdk_config_INCLUDE_DIR}")
-  if(NOT dpdk_config_INCLUDE_DIR STREQUAL dpdk_common_INCLUDE_DIR)
-    list(APPEND dpdk_INCLUDE_DIRS "${dpdk_common_INCLUDE_DIR}")
-  endif()
-
-  set(components
-    bus_pci
-    cmdline
-    eal
-    ethdev
-    hash
-    kvargs
-    mbuf
-    mempool
-    mempool_ring
-    mempool_stack
-    pci
-    pmd_af_packet
-    pmd_bond
-    pmd_i40e
-    pmd_ixgbe
-    pmd_mlx5
-    pmd_ring
-    pmd_vmxnet3_uio
-    ring)
-
-  set(dpdk_LIBRARIES)
-
-  foreach(c ${components})
-    find_library(DPDK_rte_${c}_LIBRARY rte_${c}
-      HINTS
-        ENV DPDK_DIR
-        ${dpdk_LIBRARY_DIRS}
-      PATH_SUFFIXES lib)
-    if(DPDK_rte_${c}_LIBRARY)
-      set(dpdk_lib dpdk::${c})
-      if (NOT TARGET ${dpdk_lib})
-        add_library(${dpdk_lib} UNKNOWN IMPORTED)
-        set_target_properties(${dpdk_lib} PROPERTIES
-          INTERFACE_INCLUDE_DIRECTORIES "${dpdk_INCLUDE_DIRS}"
-          IMPORTED_LOCATION "${DPDK_rte_${c}_LIBRARY}")
-        if(c STREQUAL pmd_mlx5)
-          find_package(verbs QUIET)
-          if(verbs_FOUND)
-            target_link_libraries(${dpdk_lib} INTERFACE IBVerbs::verbs)
-          endif()
-        endif()
-      endif()
-      list(APPEND dpdk_LIBRARIES ${dpdk_lib})
-    endif()
-  endforeach()
-
-  #
-  # Where the heck did this list come from?  libdpdk on Ubuntu 20.04,
-  # for example, doesn't even *have* -ldpdk; that's why we go with
-  # pkg-config, in the hopes that it provides a correct set of flags
-  # for this tangled mess.
-  #
-  list(APPEND dpdk_LIBRARIES dpdk rt m numo dl)
 endif()
 
 mark_as_advanced(dpdk_INCLUDE_DIRS ${dpdk_LIBRARIES})
