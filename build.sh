661d5b9ac73181fd7bbaa2890d429d6cc0a5027f
Denis Ovsienko <denis@ovsienko.info>
2022-07-06 16:20:41 +0100

    Note more Haiku warnings in build.sh. [skip ci]

--- build.sh
+++ build.sh
@@ -79,6 +79,7 @@ suncc-5.1[45]/SunOS-5.11)
     LIBPCAP_TAINTED=yes
     ;;
 */Haiku-*)
+    # (GCC 8.3.0 and later, Clang 9.0.1.)
     # pcap-haiku.cpp:55:21: warning: unused variable 'handlep' [-Wunused-variable]
     # pcap-haiku.cpp:50:37: warning: unused parameter 'maxPackets' [-Wunused-parameter]
     # pcap-haiku.cpp:111:47: warning: unused parameter 'buffer' [-Wunused-parameter]
@@ -86,6 +87,21 @@ suncc-5.1[45]/SunOS-5.11)
     # pcap-haiku.cpp:268:26: warning: unused parameter 'name' [-Wunused-parameter]
     # pcap-haiku.cpp:274:26: warning: unused parameter 'name' [-Wunused-parameter]
     # pcap-haiku.cpp:274:58: warning: unused parameter 'errbuf' [-Wunused-parameter]
+    #
+    # (The warnings below come from GCC and Clang in CMake builds after installing
+    # all system updates.)
+    # gencode.c:4143:9: warning: converting a packed 'struct in6_addr' pointer
+    #   (alignment 1) to a 'uint32_t' {aka 'unsigned int'} pointer (alignment 4) may
+    #   result in an unaligned pointer value [-Waddress-of-packed-member]
+    # gencode.c:4144:9: warning: converting a packed 'struct in6_addr' pointer
+    #   (alignment 1) to a 'uint32_t' {aka 'unsigned int'} pointer (alignment 4) may
+    #   result in an unaligned pointer value [-Waddress-of-packed-member]
+    # gencode.c:7189:9: warning: converting a packed 'struct in6_addr' pointer
+    #   (alignment 1) to a 'uint32_t' {aka 'unsigned int'} pointer (alignment 4) may
+    #   result in an unaligned pointer value [-Waddress-of-packed-member]
+    # gencode.c:7190:9: warning: converting a packed 'struct in6_addr' pointer
+    #   (alignment 1) to a 'uint32_t' {aka 'unsigned int'} pointer (alignment 4) may
+    #   result in an unaligned pointer value [-Waddress-of-packed-member]
     LIBPCAP_TAINTED=yes
     ;;
 esac
