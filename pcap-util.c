4006cf0d18915a2a974aa9fb50e3388bce609dc0
Denis Ovsienko <denis@ovsienko.info>
2023-01-23 23:02:18 +0000

    Fix a comment in pcap-util.c. [skip ci]

--- pcap-util.c
+++ pcap-util.c
@@ -18,7 +18,7 @@
  * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  *
- * pcap-common.c - common code for pcap and pcapng files
+ * pcap-util.c - common code for various files
  */
 
 #ifdef HAVE_CONFIG_H
