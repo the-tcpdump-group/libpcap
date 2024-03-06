the-tcpdump-group/libpcap

commit ec5243914b78a402de51253de8c6f578552672b6

Print MAC addresses in findalldevstest. [skip appveyor]
If AF_PACKET or AF_LINK is defined, include the headers and enable the
code required to print at least the basic properties of the address
family.  If the type and the length of the address match Ethernet, print
the [MAC] address.  Mask the last three octets of MAC addresses unless
the UNMASK_MAC_ADDRESSES environment variable is set to "yes".

testprogs/findalldevstest.c
@@ -15,6 +15,28 @@
   #include <arpa/inet.h>
   #include <netdb.h>
   #include <unistd.h>
+
+  // Different OSes define ETHER_ADDR_LEN in different headers, if at all, so
+  // it would take an amount of conditionals similar to that in nametoaddr.c
+  // just to get the well-known constant from an OS (or not).  Keep it simple.
+  #ifndef ETHER_ADDR_LEN
+    #define ETHER_ADDR_LEN 6
+  #endif // ETHER_ADDR_LEN
+
+  // Linux defines and uses AF_PACKET.
+  // FreeBSD, Haiku, NetBSD and OpenBSD define and use AF_LINK.
+  // illumos defines both AF_PACKET and AF_LINK, and uses AF_LINK.
+  // Solaris 11 defines both AF_PACKET and AF_LINK, but uses neither.
+  // GNU/Hurd defines neither AF_PACKET nor AF_LINK.
+  #include <net/if.h>
+  #ifdef AF_PACKET
+    #include <netpacket/packet.h> // struct sockaddr_ll
+    #include <net/if_arp.h> // ARPHRD_ETHER
+  #endif // AF_PACKET
+  #ifdef AF_LINK
+    #include <net/if_dl.h> // struct sockaddr_dl
+    #include <net/if_types.h> // IFT_ETHER
+  #endif // AF_LINK
 #endif
 
 #include <pcap.h>
@@ -178,6 +200,22 @@ int main(int argc, char **argv)
   exit(exit_status);
 }
 
+#if defined(AF_PACKET) || defined(AF_LINK)
+static const char *
+mac_addr_str(const u_char addr[])
+{
+  static char buffer[] = "00:00:00:00:00:00";
+  const char *unmask = getenv("UNMASK_MAC_ADDRESSES");
+  if (!unmask || strcmp("yes", unmask))
+    snprintf(buffer, sizeof(buffer), "%02x:%02x:%02x:xx:xx:xx",
+             addr[0], addr[1], addr[2]);
+  else
+    snprintf(buffer, sizeof(buffer), "%02x:%02x:%02x:%02x:%02x:%02x",
+             addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
+  return buffer;
+}
+#endif // defined(AF_PACKET) || defined(AF_LINK)
+
 static int ifprint(pcap_if_t *d)
 {
   pcap_addr_t *a;
@@ -272,6 +310,40 @@ static int ifprint(pcap_if_t *d)
                &((struct sockaddr_in *)(a->dstaddr))->sin_addr,
                ipv4_buf, sizeof ipv4_buf));
         break;
+
+#ifdef AF_PACKET
+      case AF_PACKET:
+        printf("\tAddress Family: AF_PACKET (%d)\n", a->addr->sa_family);
+        struct sockaddr_ll *sll = (struct sockaddr_ll *)a->addr;
+        printf("\t\tInterface Index: %u\n", sll->sll_ifindex);
+        printf("\t\tType: %d%s\n", sll->sll_hatype,
+               sll->sll_hatype == ARPHRD_ETHER ? " (ARPHRD_ETHER)" : "");
+        printf("\t\tLength: %u\n", sll->sll_halen);
+        if (sll->sll_hatype == ARPHRD_ETHER && sll->sll_halen == ETHER_ADDR_LEN)
+          printf("\t\tAddress: %s\n",
+                 mac_addr_str((const u_char *)sll->sll_addr));
+      break;
+#endif // AF_PACKET
+
+#ifdef AF_LINK
+      case AF_LINK:
+        printf("\tAddress Family: AF_LINK (%d)\n", a->addr->sa_family);
+        struct sockaddr_dl *sdl = (struct sockaddr_dl *)a->addr;
+        printf("\t\tInterface Index: %u\n", sdl->sdl_index);
+        printf("\t\tType: %u%s\n", sdl->sdl_type,
+               sdl->sdl_type == IFT_ETHER ? " (IFT_ETHER)" : "");
+        printf("\t\tLength: %u\n", sdl->sdl_alen);
+#ifdef __illumos__
+        // Could be a bug or a peculiarity of the API.
+        if (sdl->sdl_type == 0 && sdl->sdl_alen == ETHER_ADDR_LEN)
+#else
+        if (sdl->sdl_type == IFT_ETHER && sdl->sdl_alen == ETHER_ADDR_LEN)
+#endif
+          printf("\t\tAddress: %s\n",
+                 mac_addr_str((const u_char *)sdl->sdl_data));
+      break;
+#endif // AF_LINK
+
 #ifdef INET6
       case AF_INET6:
         printf("\tAddress Family: AF_INET6 (%d)\n", a->addr->sa_family);

commit 81a277ef13e8d9a570b0433892ddbdba5fcb9a8d

Make more code cleanups in findalldevstest.c.
As it turns out, my commit ec52439 caused breakage in Windows builds:

findalldevstest.c(335): error C2039: 'sdl_index': is not a member of
  'sockaddr_dl'
findalldevstest.c(336): error C2039: 'sdl_type': is not a member of
  'sockaddr_dl'
findalldevstest.c(337): error C2039: 'sdl_type': is not a member of
  'sockaddr_dl'
findalldevstest.c(337): error C2065: 'IFT_ETHER': undeclared identifier
findalldevstest.c(338): error C2039: 'sdl_alen': is not a member of
  'sockaddr_dl'
findalldevstest.c(343): error C2039: 'sdl_type': is not a member of
  'sockaddr_dl'
findalldevstest.c(343): error C2065: 'IFT_ETHER': undeclared identifier
findalldevstest.c(343): error C2039: 'sdl_alen': is not a member of
  'sockaddr_dl'
findalldevstest.c(343): error C2065: 'ETHER_ADDR_LEN': undeclared
  identifier

As Guy Harris explains, certain versions of Windows define AF_LINK, but
struct sockaddr_dl is different.  Consistently disable all AF_PACKET and
AF_LINK handling code on Windows to fix this.

The INET6 macro controls IPv6-specific features of the filter syntax,
but not the set of address families pcap_findalldevs() can return.  Thus
in ifprint() make the code that prints an AF_INET6 address conditional
on AF_INET6, not INET6.

Ibid., put the short branch of a long if statement first; move some
variables closer to the code that needs them; lose a no-op statement; do
not test whether .addr is not NULL because the entire switch statement
is already conditional on that.

Rewrite mac_addr_str() as ether_ntop() to make printing of AF_PACKET and
AF_LINK more consistent with AF_INET and AF_INET6.

testprogs/findalldevstest.c
@@ -200,29 +200,22 @@ int main(int argc, char **argv)
   exit(exit_status);
 }
 
-#if defined(AF_PACKET) || defined(AF_LINK)
-static const char *
-mac_addr_str(const u_char addr[])
+#if ! defined(_WIN32) && (defined(AF_PACKET) || defined(AF_LINK))
+static char *
+ether_ntop(const u_char addr[], char *buffer, size_t size, u_char mask)
 {
-  static char buffer[] = "00:00:00:00:00:00";
-  const char *unmask = getenv("UNMASK_MAC_ADDRESSES");
-  if (!unmask || strcmp("yes", unmask))
-    snprintf(buffer, sizeof(buffer), "%02x:%02x:%02x:xx:xx:xx",
+  if (mask)
+    snprintf(buffer, size, "%02x:%02x:%02x:xx:xx:xx",
              addr[0], addr[1], addr[2]);
   else
-    snprintf(buffer, sizeof(buffer), "%02x:%02x:%02x:%02x:%02x:%02x",
+    snprintf(buffer, size, "%02x:%02x:%02x:%02x:%02x:%02x",
              addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
   return buffer;
 }
-#endif // defined(AF_PACKET) || defined(AF_LINK)
+#endif // ! defined(_WIN32) && (defined(AF_PACKET) || defined(AF_LINK))
 
 static int ifprint(pcap_if_t *d)
 {
-  pcap_addr_t *a;
-  char ipv4_buf[INET_ADDRSTRLEN];
-#ifdef INET6
-  char ipv6_buf[INET6_ADDRSTRLEN];
-#endif
   const char *sep;
   int status = 1; /* success */
 
@@ -281,19 +274,28 @@ static int ifprint(pcap_if_t *d)
       break;
     }
   }
-  sep = ", ";
   printf("\n");
 
-  for(a=d->addresses;a;a=a->next) {
-    if (a->addr != NULL)
+  const char *unmask = getenv("UNMASK_MAC_ADDRESSES");
+  if (unmask && strcmp("yes", unmask))
+    unmask = NULL;
+
+  for (pcap_addr_t *a = d->addresses; a; a=a->next) {
+    if (a->addr == NULL) {
+      fprintf(stderr, "\tWarning: a->addr is NULL, skipping this address.\n");
+      status = 0;
+    } else {
+#if ! defined(_WIN32) && (defined(AF_PACKET) || defined(AF_LINK))
+      char ether_buf[] = "00:00:00:00:00:00";
+#endif // ! defined(_WIN32) && (defined(AF_PACKET) || defined(AF_LINK))
       switch(a->addr->sa_family) {
       case AF_INET:
         printf("\tAddress Family: AF_INET (%d)\n", a->addr->sa_family);
-        if (a->addr)
-          printf("\t\tAddress: %s\n",
-            inet_ntop(AF_INET,
-               &((struct sockaddr_in *)(a->addr))->sin_addr,
-               ipv4_buf, sizeof ipv4_buf));
+        char ipv4_buf[INET_ADDRSTRLEN];
+        printf("\t\tAddress: %s\n",
+          inet_ntop(AF_INET,
+             &((struct sockaddr_in *)(a->addr))->sin_addr,
+             ipv4_buf, sizeof ipv4_buf));
         if (a->netmask)
           printf("\t\tNetmask: %s\n",
             inet_ntop(AF_INET,
@@ -311,6 +313,7 @@ static int ifprint(pcap_if_t *d)
                ipv4_buf, sizeof ipv4_buf));
         break;
 
+#if ! defined(_WIN32)
 #ifdef AF_PACKET
       case AF_PACKET:
         printf("\tAddress Family: AF_PACKET (%d)\n", a->addr->sa_family);
@@ -321,7 +324,8 @@ static int ifprint(pcap_if_t *d)
         printf("\t\tLength: %u\n", sll->sll_halen);
         if (sll->sll_hatype == ARPHRD_ETHER && sll->sll_halen == ETHER_ADDR_LEN)
           printf("\t\tAddress: %s\n",
-                 mac_addr_str((const u_char *)sll->sll_addr));
+                 ether_ntop((const u_char *)sll->sll_addr,
+                            ether_buf, sizeof(ether_buf), ! unmask));
       break;
 #endif // AF_PACKET
 
@@ -338,20 +342,22 @@ static int ifprint(pcap_if_t *d)
         if (sdl->sdl_type == 0 && sdl->sdl_alen == ETHER_ADDR_LEN)
 #else
         if (sdl->sdl_type == IFT_ETHER && sdl->sdl_alen == ETHER_ADDR_LEN)
-#endif
+#endif // __illumos__
           printf("\t\tAddress: %s\n",
-                 mac_addr_str((const u_char *)sdl->sdl_data));
+                 ether_ntop((const u_char *)sdl->sdl_data,
+                            ether_buf, sizeof(ether_buf), ! unmask));
       break;
 #endif // AF_LINK
+#endif // ! defined(_WIN32)
 
-#ifdef INET6
+#ifdef AF_INET6
       case AF_INET6:
         printf("\tAddress Family: AF_INET6 (%d)\n", a->addr->sa_family);
-        if (a->addr)
-          printf("\t\tAddress: %s\n",
-            inet_ntop(AF_INET6,
-               ((struct sockaddr_in6 *)(a->addr))->sin6_addr.s6_addr,
-               ipv6_buf, sizeof ipv6_buf));
+        char ipv6_buf[INET6_ADDRSTRLEN];
+        printf("\t\tAddress: %s\n",
+          inet_ntop(AF_INET6,
+             ((struct sockaddr_in6 *)(a->addr))->sin6_addr.s6_addr,
+             ipv6_buf, sizeof ipv6_buf));
         if (a->netmask)
           printf("\t\tNetmask: %s\n",
             inet_ntop(AF_INET6,
@@ -368,17 +374,13 @@ static int ifprint(pcap_if_t *d)
               ((struct sockaddr_in6 *)(a->dstaddr))->sin6_addr.s6_addr,
                ipv6_buf, sizeof ipv6_buf));
         break;
-#endif
+#endif // AF_INET6
       default:
         printf("\tAddress Family: Unknown (%d)\n", a->addr->sa_family);
         break;
-      }
-    else
-    {
-      fprintf(stderr, "\tWarning: a->addr is NULL, skipping this address.\n");
-      status = 0;
-    }
-  }
+      } // switch
+    } // if
+  } // for
   printf("\n");
   return status;
 }