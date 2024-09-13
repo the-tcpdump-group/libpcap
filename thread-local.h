61ed382a58f42981210750ec5d72ed8839abe8c7
Michael Osipov <michael.osipov@siemens.com>
2024-09-13 17:26:53 +0200

    Add thread_local macro for HP-UX

--- thread-local.h
+++ thread-local.h
@@ -62,7 +62,8 @@
   /* note that ICC (linux) and Clang are covered by __GNUC__ */
   #elif defined __GNUC__ || \
          defined __SUNPRO_C || \
-         defined __xlC__
+         defined __xlC__ || \
+         defined __hpux
     #define thread_local __thread
   #else
     #error "Cannot define thread_local"
