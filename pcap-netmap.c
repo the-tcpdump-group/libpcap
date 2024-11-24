630b3a19cac0efcdc9fb06a773be02efc8c13980
Denis Ovsienko <denis@ovsienko.info>
2024-11-24 18:31:29 +0000

    Use __func__ in pcap-netmap.c. [skip ci]

--- pcap-netmap.c
+++ pcap-netmap.c
@@ -235,7 +235,7 @@ pcap_netmap_activate(pcap_t *p)
 	}
 #if 0
 	fprintf(stderr, "%s device %s priv %p fd %d ports %d..%d\n",
-	    __FUNCTION__, p->opt.device, d, d->fd,
+	    __func__, p->opt.device, d, d->fd,
 	    d->first_rx_ring, d->last_rx_ring);
 #endif
 	pn->d = d;
