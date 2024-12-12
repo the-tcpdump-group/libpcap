d0bfeaecfaf570cd980326d88c92ee949d15a586
Denis Ovsienko <denis@ovsienko.info>
2024-12-12 20:32:49 +0000

    CI: Implement "make check". [skip appveyor]
    Introduce a Perl script with the same name and semantics as in tcpdump
    and transcribe a number of filter expressions.  In the build matrix
    scripts run the new script (using Valgrind if available).

--- build_matrix.sh
+++ build_matrix.sh
@@ -40,6 +40,9 @@ if command -v valgrind >/dev/null 2>&1; then
     valgrind --version
     VALGRIND_CMD="valgrind --leak-check=full --error-exitcode=1 --quiet"
     export VALGRIND_CMD
+    # With Valgrind filtertest takes significantly longer to complete.
+    : "${FILTERTEST_TIMEOUT:=5}"
+    export FILTERTEST_TIMEOUT
 fi
 
 run_after_echo git show --oneline -s | cat
