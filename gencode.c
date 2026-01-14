the-tcpdump-group/libpcap

commit bea13af90fed8a01ab74dc037a3e5bf804a199da

Improve the DLT_PFLOG corner case in gen_host().

gencode.c
@@ -5165,11 +5165,30 @@ gen_host(compiler_state_t *cstate, bpf_u_int32 addr, bpf_u_int32 mask,
 
 	case Q_RARP:
 		b0 = gen_linktype(cstate, ETHERTYPE_REVARP);
+		/*
+		 * If this DLT does not support RARP, the result of
+		 * gen_linktype() is a Boolean false, then the subsequent
+		 * gen_and() would discard the result of gen_hostop() and
+		 * return the Boolean false.
+		 *
+		 * However, if this DLT also uses a variable-length link-layer
+		 * header (which means DLT_PFLOG only at the time of this
+		 * writing), a side effect of the gen_hostop() invocation
+		 * would be registering a demand for a variable-length offset
+		 * preamble, which a Boolean constant never needs, so in this
+		 * case return early and have one fewer reasons to produce the
+		 * preamble in insert_compute_vloffsets().
+		 */
+		if (b0->meaning == IS_FALSE)
+			return b0;
 		b1 = gen_hostop(cstate, addr, mask, dir, 14, 24);
 		return gen_and(b0, b1);
 
 	case Q_ARP:
 		b0 = gen_linktype(cstate, ETHERTYPE_ARP);
+		// same as for Q_RARP
+		if (b0->meaning == IS_FALSE)
+			return b0;
 		b1 = gen_hostop(cstate, addr, mask, dir, 14, 24);
 		return gen_and(b0, b1);
 

testprogs/TESTrun
@@ -6078,15 +6078,10 @@ my @accept_blocks = (
 		# The "opt" leg of this block is a reject test.
 		opt => undef,
 		unopt => '
-			(000) ldb      [0]
-			(001) add      #3
-			(002) and      #0xfffffffc
-			(003) st       M[0]
-			(004) tax
-			(005) ld       #0x1
-			(006) jeq      #0x0             jt 7	jf 8
-			(007) ret      #262144
-			(008) ret      #0
+			(000) ld       #0x1
+			(001) jeq      #0x0             jt 2	jf 3
+			(002) ret      #262144
+			(003) ret      #0
 			',
 	}, # arp_host_addr_PFLOG
 	# gen_host() -> case Q_RARP
@@ -6097,15 +6092,10 @@ my @accept_blocks = (
 		# The "opt" leg of this block is a reject test.
 		opt => undef,
 		unopt => '
-			(000) ldb      [0]
-			(001) add      #3
-			(002) and      #0xfffffffc
-			(003) st       M[0]
-			(004) tax
-			(005) ld       #0x1
-			(006) jeq      #0x0             jt 7	jf 8
-			(007) ret      #262144
-			(008) ret      #0
+			(000) ld       #0x1
+			(001) jeq      #0x0             jt 2	jf 3
+			(002) ret      #262144
+			(003) ret      #0
 			',
 	}, # rarp_host_addr_PFLOG
 

commit 1daf61c4a59c380a0a025f21cec7cac099c60eec

Fix a bug in the earlier DLT_PFLOG optimization.
My earlier commit bea13af introduced a bug: when in the Q_ARP and Q_RARP
cases the conditional early return skips the invocation of gen_hostop(),
it removes one unwanted side effect, but also another side effect, which
is the wanted validation of dir qualifier.  This caused a failure to
reject filter expressions that have "arp" or "rarp" as the proto
qualifier, a WLAN dir qualifier, "host" or "net" as the type qualifier
and an ID that is either an IPv4 address or an IPv4 CIDR or a network
name or a hostname that resolves to at least one IPv4 address, for
example:

$ filtertest -O IPV4 'arp addr1 net 10.0.0.0/8'
(000) ld       #0x1
(001) jeq      #0x0             jt 2	jf 3
(002) ret      #262144
(003) ret      #0

To fix this, in gen_host(), gen_host6(), gen_dnhost() and gen_acode()
reject all WLAN dir qualifiers early.  In gen_hostop(), gen_hostop6(),
gen_dnhostop() and gen_mac8host() convert the existing default cases to
generate an internal error because now these default cases should never
match.  In gen_mac48host() do both unless the code path diverges to
gen_wlanhostop():

$ filtertest -O IPV4 'arp addr1 net 10.0.0.0/8'
filtertest: 'addr1' is valid for 802.11 syntax only

The [ARP/RARP-specific] regression did not fail any of the existing
tests, which correspond to the Q_IP code path in gen_host(), which
always invoked gen_hostop(), so in TESTrun expand the reject tests
combinatorial space to cover more code paths.

gencode.c
@@ -1111,11 +1111,26 @@ assert_maxval(compiler_state_t *cstate, const char *name,
 		    name, val, maxval);
 }
 
-#define ERRSTR_802_11_ONLY_KW "'%s' is valid for 802.11 syntax only"
+static void
+assert_nonwlan_dqual(compiler_state_t *cstate, const u_char dir)
+{
+	switch (dir) {
+	case Q_SRC:
+	case Q_DST:
+	case Q_AND:
+	case Q_DEFAULT:
+	case Q_OR:
+		break;
+	default:
+		bpf_error(cstate, "'%s' is valid for 802.11 syntax only", dqkw(dir));
+	}
+}
+
 #define ERRSTR_INVALID_QUAL "'%s' is not a valid qualifier for '%s'"
 #define ERRSTR_UNKNOWN_MAC48HOST "unknown Ethernet-like host '%s'"
 #define ERRSTR_INVALID_IPV4_ADDR "invalid IPv4 address '%s'"
 #define ERRSTR_FUNC_VAR_INT "internal error in %s(): %s == %d"
+#define ERRSTR_FUNC_VAR_STR "internal error in %s(): %s == '%s'"
 
 // Validate a port/portrange proto qualifier and map to an IP protocol number.
 static int
@@ -4516,7 +4531,8 @@ gen_hostop(compiler_state_t *cstate, bpf_u_int32 addr, bpf_u_int32 mask,
 		return gen_or(b0, b1);
 
 	default:
-		bpf_error(cstate, ERRSTR_802_11_ONLY_KW, dqkw(dir));
+		// Bug: a WLAN dqual should have been rejected earlier.
+		bpf_error(cstate, ERRSTR_FUNC_VAR_STR, __func__, "dir", dqkw(dir));
 		/*NOTREACHED*/
 	}
 	return gen_mcmp(cstate, OR_LINKPL, offset, BPF_W, addr, mask);
@@ -4561,7 +4577,8 @@ gen_hostop6(compiler_state_t *cstate, struct in6_addr *addr,
 		return gen_or(b0, b1);
 
 	default:
-		bpf_error(cstate, ERRSTR_802_11_ONLY_KW, dqkw(dir));
+		// Bug: a WLAN dqual should have been rejected earlier.
+		bpf_error(cstate, ERRSTR_FUNC_VAR_STR, __func__, "dir", dqkw(dir));
 		/*NOTREACHED*/
 	}
 	/* this order is important */
@@ -4999,7 +5016,8 @@ gen_dnhostop(compiler_state_t *cstate, bpf_u_int32 addr, int dir)
 		return gen_or(b0, b1);
 
 	default:
-		bpf_error(cstate, ERRSTR_802_11_ONLY_KW, dqkw(dir));
+		// Bug: a WLAN dqual should have been rejected earlier.
+		bpf_error(cstate, ERRSTR_FUNC_VAR_STR, __func__, "dir", dqkw(dir));
 		/*NOTREACHED*/
 	}
 	/*
@@ -5105,6 +5123,14 @@ static struct block *
 gen_host(compiler_state_t *cstate, bpf_u_int32 addr, bpf_u_int32 mask,
     int proto, int dir, int type)
 {
+	/*
+	 * WLAN direction qualifiers are never valid for IPv4 addresses.
+	 *
+	 * It is important to validate this now because the call to
+	 * gen_hostop() may be optimized out below.
+	 */
+	assert_nonwlan_dqual(cstate, dir);
+
 	struct block *b0, *b1;
 
 	switch (proto) {
@@ -5166,6 +5192,9 @@ static struct block *
 gen_host6(compiler_state_t *cstate, struct in6_addr *addr,
     struct in6_addr *mask, int proto, int dir, int type)
 {
+	// WLAN direction qualifiers are never valid for IPv6 addresses.
+	assert_nonwlan_dqual(cstate, dir);
+
 	struct block *b0, *b1;
 
 	switch (proto) {
@@ -5252,6 +5281,9 @@ static struct block *
 gen_dnhost(compiler_state_t *cstate, const char *s, bpf_u_int32 v,
     const struct qual q)
 {
+	// WLAN direction qualifiers are never valid for DECnet addresses.
+	assert_nonwlan_dqual(cstate, q.dir);
+
 	/*
 	 * libpcap defines exactly one primitive that has "decnet" as
 	 * the protocol qualifier: "decnet host AREANUMBER.NODENUMBER".
@@ -5317,6 +5349,10 @@ gen_mac48host(compiler_state_t *cstate, const u_char *eaddr, const u_char dir,
 	struct block *b1 = NULL;
 	u_int src_off, dst_off;
 
+	/*
+	 * Do not validate dir yet and let gen_wlanhostop() handle the DLTs
+	 * that support WLAN direction qualifiers.
+	 */
 	switch (cstate->linktype) {
 	case DLT_EN10MB:
 	case DLT_NETANALYZER:
@@ -5359,6 +5395,8 @@ gen_mac48host(compiler_state_t *cstate, const u_char *eaddr, const u_char dir,
 	default:
 		fail_kw_on_dlt(cstate, keyword);
 	}
+	// Now validate.
+	assert_nonwlan_dqual(cstate, dir);
 
 	struct block *b0, *tmp;
 
@@ -5381,7 +5419,8 @@ gen_mac48host(compiler_state_t *cstate, const u_char *eaddr, const u_char dir,
 		b0 = gen_or(tmp, b0);
 		break;
 	default:
-		bpf_error(cstate, ERRSTR_802_11_ONLY_KW, dqkw(dir));
+		// Bug: a WLAN dqual should have been rejected earlier.
+		bpf_error(cstate, ERRSTR_FUNC_VAR_STR, __func__, "dir", dqkw(dir));
 	}
 
 	return b1 ? gen_and(b1, b0) : b0;
@@ -5454,7 +5493,8 @@ gen_mac8host(compiler_state_t *cstate, const uint8_t mac8, const u_char dir,
 		dst = gen_cmp(cstate, OR_LINKHDR, dst_off, BPF_B, mac8);
 		return gen_or(src, dst);
 	default:
-		bpf_error(cstate, ERRSTR_INVALID_QUAL, dqkw(dir), context);
+		// Bug: a WLAN dqual should have been rejected earlier.
+		bpf_error(cstate, ERRSTR_FUNC_VAR_STR, __func__, "dir", dqkw(dir));
 	}
 }
 
@@ -7195,6 +7235,9 @@ gen_acode(compiler_state_t *cstate, const char *s, struct qual q)
 	 */
 	if (setjmp(cstate->top_ctx))
 		return (NULL);
+
+	// WLAN direction qualifiers are never valid for MAC-8 addresses.
+	assert_nonwlan_dqual(cstate, q.dir);
 
 	if (q.addr != Q_HOST && q.addr != Q_DEFAULT)
 		bpf_error(cstate, ERRSTR_INVALID_QUAL, tqkw(q.addr), "$XX");

testprogs/TESTrun
@@ -20358,7 +20358,7 @@ sub errstr_nomac48host {
 	return sprintf ('unknown Ethernet-like host \'%s\'', shift);
 }
 
-# ERRSTR_802_11_ONLY_KW
+# assert_nonwlan_dqual()
 sub errstr_invkw_notwlan {
 	return sprintf ('\'%s\' is valid for 802.11 syntax only', shift);
 }
@@ -22694,17 +22694,35 @@ foreach (DLTs_without 'wlan') {
 	};
 }
 
-foreach my $DLT (DLTs_without 'wlan') {
-	next if ! DLT_feature $DLT, 'link_proto';
+foreach my $DLT (DLTs_with 'link_proto') {
 	foreach my $dqual (dquals_with 'wlan') {
-		# HID -> gen_ncode() -> gen_host() -> gen_hostop() -> default case
+		# HID -> gen_ncode() -> gen_host() -> assert_nonwlan_dqual()
 		push @filter_reject_tests, {
 			name => "${dqual}_ipv4addr_${DLT}",
 			DLT => $DLT,
 			expr => "$dqual 1.2.3.4",
 			errstr => errstr_invkw_notwlan ($dqual),
 		};
-		# gen_scode() -> case Q_NET -> gen_host() -> gen_hostop() -> default case
+		push @filter_reject_tests, {
+			name => "${_}_${dqual}_ipv4addr_${DLT}",
+			DLT => $DLT,
+			expr => "$_ $dqual 1.2.3.4",
+			errstr => errstr_invkw_notwlan ($dqual),
+		} foreach qw(ip arp rarp);
+		# HID / NUM -> gen_mcode() -> gen_host() -> assert_nonwlan_dqual()
+		push @filter_reject_tests, {
+			name => "${dqual}_net_ipv4cidr_${DLT}",
+			DLT => $DLT,
+			expr => "$dqual net 10.20.30.240/28",
+			errstr => errstr_invkw_notwlan ($dqual),
+		};
+		push @filter_reject_tests, {
+			name => "${_}_${dqual}_net_ipv4cidr_${DLT}",
+			DLT => $DLT,
+			expr => "$_ $dqual net 10.20.30.240/28",
+			errstr => errstr_invkw_notwlan ($dqual),
+		} foreach qw(ip arp rarp);
+		# gen_scode() -> case Q_NET -> gen_host() -> assert_nonwlan_dqual()
 		push @filter_reject_tests, {
 			name => "${dqual}_net_name_${DLT}",
 			skip => skip_no_networks(),
@@ -22712,7 +22730,14 @@ foreach my $DLT (DLTs_without 'wlan') {
 			expr => "$dqual net net-10-20-30-0.libpcap.test",
 			errstr => errstr_invkw_notwlan ($dqual),
 		};
-		# gen_scode() -> case Q_HOST -> q.proto != Q_LINK -> gen_host46_byname() -> gen_host() -> gen_hostop() -> default case
+		push @filter_reject_tests, {
+			name => "${_}_${dqual}_net_name_${DLT}",
+			skip => skip_no_networks(),
+			DLT => $DLT,
+			expr => "$_ $dqual net net-10-20-30-0.libpcap.test",
+			errstr => errstr_invkw_notwlan ($dqual),
+		} foreach qw(ip arp rarp);
+		# gen_scode() -> gen_host46_byname() -> gen_host() -> assert_nonwlan_dqual()
 		push @filter_reject_tests, {
 			name => "${dqual}_noeth_ipv4_noipv6_${DLT}",
 			skip => skip_no_hosts(),
@@ -22720,15 +22745,28 @@ foreach my $DLT (DLTs_without 'wlan') {
 			expr => "$dqual noeth-ipv4-noipv6.host123.libpcap.test",
 			errstr => errstr_invkw_notwlan ($dqual),
 		};
+		push @filter_reject_tests, {
+			name => "${_}_${dqual}_noeth_ipv4_noipv6_${DLT}",
+			skip => skip_no_hosts(),
+			DLT => $DLT,
+			expr => "$_ $dqual noeth-ipv4-noipv6.host123.libpcap.test",
+			errstr => errstr_invkw_notwlan ($dqual),
+		} foreach qw(ip arp rarp);
 
-		# HID6 -> gen_mcode6() -> gen_host6() -> gen_hostop6() -> default case
+		# HID6 -> gen_mcode6() -> gen_host6() -> assert_nonwlan_dqual()
 		push @filter_reject_tests, {
 			name => "${dqual}_host_ipv6addr_${DLT}",
 			DLT => $DLT,
 			expr => "$dqual host 1234::",
 			errstr => errstr_invkw_notwlan ($dqual),
 		};
-		# gen_scode() -> case Q_HOST -> q.proto != Q_LINK -> gen_host46_byname() -> gen_host6() -> gen_hostop6() -> default case
+		push @filter_reject_tests, {
+			name => "ip6_${dqual}_host_ipv6addr_${DLT}",
+			DLT => $DLT,
+			expr => "ip6 $dqual host 1234::",
+			errstr => errstr_invkw_notwlan ($dqual),
+		};
+		# gen_scode() -> gen_host46_byname() -> gen_host6() -> assert_nonwlan_dqual()
 		push @filter_reject_tests, {
 			name => "${dqual}_noeth_noipv4_ipv6_${DLT}",
 			skip => skip_no_hosts(),
@@ -22736,8 +22774,15 @@ foreach my $DLT (DLTs_without 'wlan') {
 			expr => "$dqual noeth-noipv4-ipv6.host123.libpcap.test",
 			errstr => errstr_invkw_notwlan ($dqual),
 		};
+		push @filter_reject_tests, {
+			name => "ip6_${dqual}_noeth_noipv4_ipv6_${DLT}",
+			skip => skip_no_hosts(),
+			DLT => $DLT,
+			expr => "ip6 $dqual noeth-noipv4-ipv6.host123.libpcap.test",
+			errstr => errstr_invkw_notwlan ($dqual),
+		};
 
-		# HID -> gen_ncode() -> gen_dnhost() -> gen_dnhostop() -> default case
+		# HID -> gen_ncode() -> gen_dnhost() -> assert_nonwlan_dqual()
 		push @filter_reject_tests, {
 			name => "decnet_${dqual}_dnaddr_${DLT}",
 			DLT => $DLT,
@@ -22785,14 +22830,14 @@ foreach my $DLT (DLTs_without 'wlan') {
 foreach my $DLT (DLTs_without 'wlan') {
 	next if ! DLT_feature $DLT, 'link_host_mac48';
 	foreach my $dqual (dquals_with 'wlan') {
-		# EID -> gen_ecode() -> gen_mac48host() -> second default case
+		# EID -> gen_ecode() -> gen_mac48host() -> assert_nonwlan_dqual()
 		push @filter_reject_tests, {
 			name => "link_${dqual}_mac48_${DLT}",
 			DLT => $DLT,
 			expr => "link $dqual a:b:c:d:e:f",
 			errstr => errstr_invkw_notwlan ($dqual),
 		};
-		# gen_scode() -> case Q_HOST -> q.proto == Q_LINK -> gen_mac48host_byname() -> gen_mac48host() -> second default case
+		# gen_scode() -> gen_mac48host_byname() -> gen_mac48host() -> assert_nonwlan_dqual()
 		push @filter_reject_tests, {
 			name => "link_${dqual}_eth_noipv4_noipv6_${DLT}",
 			skip => skip_no_ethers(),
@@ -22827,12 +22872,12 @@ foreach my $DLT (DLTs_with 'link_host_mac8') {
 
 foreach my $DLT (DLTs_with 'link_host_mac8') {
 	foreach my $dqual (dquals_with 'wlan') {
-		# gen_acode() -> gen_mac8host() -> second default case
+		# gen_acode() -> assert_nonwlan_dqual()
 		push @filter_reject_tests, {
 			name => "link_${dqual}_mac8_${DLT}",
 			DLT => $DLT,
 			expr => "link $dqual \$25",
-			errstr => errstr_invqual ($dqual, 'link host $XX'),
+			errstr => errstr_invkw_notwlan ($dqual),
 		};
 	}
 }