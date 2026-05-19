6b5de1e5f07a4fea6672caa2d34935c3da24a8f2
Joao <joaonasmont@gmail.com>
2026-05-19 12:25:04 -0300

    fuzz: fix unused parameter compiler warning in fuzz_filter.c using _U_ macro from varattrs.h

--- testprogs/fuzz/fuzz_filter.c
+++ testprogs/fuzz/fuzz_filter.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "varattrs.h"
 
 #include <pcap/pcap.h>
 
-void fuzz_openFile(const char * name){
-    (void)name;//do nothing
+void fuzz_openFile(const char * name _U_){
+    //do nothing
 }
 
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
